a960067f1879b985ee41e9c83ae70c205693d17c
Vlad Riscutia <vladris@outlook.com>
2020-12-01 21:29:06 -0800

    Update 01.py

--- 2020/02/01.py
+++ 2020/02/01.py
@@ -4,7 +4,7 @@ valid = 0
 for line in lines:
     counts, letter, password = line.split()
     minc, maxc = map(int, counts.split('-'))
-    letter = letter[:-1]
+    letter = letter[0]
 
     count = sum([1 for c in password if c == letter])
     if minc <= count and count <= maxc:
