fa1125dd73a8985bcdc0d6c350b417c96a560f1f
Vlad Riscutia <vladris@outlook.com>
2020-12-01 21:28:53 -0800

    Update 02.py

--- 2020/02/02.py
+++ 2020/02/02.py
@@ -4,7 +4,7 @@ valid = 0
 for line in lines:
     pos, letter, password = line.split()
     pos1, pos2 = map(int, pos.split('-'))
-    letter = letter[:-1]
+    letter = letter[0]
 
     if (password[pos1 - 1] == letter) != (password[pos2 - 1] == letter):
         valid += 1
