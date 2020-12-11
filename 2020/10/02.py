28eaa1e5d1e84f38b7c63abbd1156dfd57fec829
Vlad Riscutia <vladris@outlook.com>
2020-12-10 16:16:35 -0800

    Day 10

--- 2020/10/02.py
+++ 2020/10/02.py
@@ -4,11 +4,10 @@ adapters = [0] + adapters + [max(adapters) + 3]
 cache = { 0: 1 }
 
 def arrangements(i):
-    if i in cache:
-        return cache[i]
+    if i not in cache:
+        cache[i] = sum([arrangements(i - j) for j in [1, 2, 3]
+            if i - j >= 0 and adapters[i] - adapters[i - j] <= 3])
 
-    cache[i] = sum([arrangements(i - j) for j in [1, 2, 3]
-        if i - j >= 0 and adapters[i] - adapters[i - j] <= 3])
     return cache[i]
 
 print(arrangements(len(adapters) - 1))
