a8aff262a3410ccec28c28ffb8237d36af17d3da
Vlad Riscutia <vladris@microsoft.com>
2020-12-15 05:51:18 -0800

    Day 15

---
+++ 2020/15/02.py
@@ -0,0 +1,14 @@
+input = [12, 20, 0, 6, 1, 17, 7]
+
+nums, last = {n: i for i, n in enumerate(input[:-1])}, input[-1]
+
+for i in range(len(input) - 1, 30000000 - 1):
+    if last in nums:
+        old = nums[last]
+        nums[last] = i
+        last = i - old
+    else:
+        nums[last] = i
+        last = 0
+
+print(last)
