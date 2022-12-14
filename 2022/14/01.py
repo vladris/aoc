9b7cd6a5768c05bb50f3b0a1635a47e991b61b57
Vlad Riscutia <vladris@outlook.com>
2022-12-14 03:46:41 -0800

    Day 14

---
+++ 2022/14/01.py
@@ -0,0 +1,55 @@
+x_walls, y_walls, sand = {}, {}, set()
+limit = 0
+for line in open('input').readlines():
+    xy = [tuple(map(int, xy.split(','))) for xy in line.strip().split(' -> ')]
+    limit = max(limit, max(xy, key=lambda c: c[1])[1])
+    for f, t in zip(xy, xy[1:]):
+        if f[0] == t[0]:
+            if f[0] not in x_walls:
+                x_walls[f[0]] = []
+            x_walls[f[0]].append((f[1], t[1]) if f[1] < t[1] else (t[1], f[1]))
+        else:
+            if f[1] not in y_walls:
+                y_walls[f[1]] = []
+            y_walls[f[1]].append((f[0], t[0]) if f[0] < t[0] else (t[0], f[0]))
+
+
+def free(x, y):
+    if (x, y) in sand:
+        return False
+
+    if x in x_walls:
+        if any(w[0] <= y <= w[1] for w in x_walls[x]):
+            return False
+
+    if y in y_walls:
+        if any(w[0] <= x <= w[1] for w in y_walls[y]):
+            return False
+    
+    return True
+
+
+def fall():
+    global limit
+    x, y = 500, 0
+
+    while y <= limit:
+        if free(x, y + 1):
+            y += 1
+        elif free(x - 1, y + 1):
+            x -= 1
+            y += 1
+        elif free(x + 1, y + 1):
+            x += 1
+            y += 1
+        else:
+            sand.add((x, y))
+            return True
+
+    return False
+
+
+while fall():
+    pass
+
+print(len(sand))
