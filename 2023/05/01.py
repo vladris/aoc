3fb939161d49efa06a6024b1307f2f27330adbd1
Vlad Riscutia <vladris@outlook.com>
2023-12-05 12:55:33 -0600

    Day 5

--- 2023/05/01.py
+++ 2023/05/01.py
@@ -2,50 +2,15 @@ inp = open('input').read()
 
 maps = inp.split('\n\n')
 
-data = [int(seed) for seed in maps[0][7:].split(' ')]
-values = list(zip(data[::2], data[1::2]))
-values = [(v[0], v[0] + v[1] - 1) for v in values]
+values = [int(seed) for seed in maps[0][7:].split(' ')]
 
 for mapping in maps[1:]:
     data = mapping.split('\n')
-    ranges = [[int(n) for n in rng.split(' ')] for rng in data[1:]]
-    ranges = [(r[1], r[1] + r[2] - 1, r[0] - r[1]) for r in ranges]
+    new_values = values[:]
+    for rng in [[int(n) for n in rng.split(' ')] for rng in data[1:]]:
+        for i in range(len(values)):
+            if rng[1] <= values[i] < rng[1] + rng[2]:
+                new_values[i] = rng[0] + (values[i] - rng[1])
+    values = new_values
 
-    new_values = []
-    for rng in ranges:
-        for v in values[:]:
-            # ( ) [ ]
-            if v[1] < rng[0]:
-                continue
-
-            # [ ] ( )
-            if v[0] > rng[1]:
-                continue
-
-            values.remove(v)
-
-            # ( [ ) ]
-            if v[0] < rng[0] and v[1] <= rng[1]:
-                values.append((v[0], rng[0] - 1))
-                new_values.append((rng[0] + rng[2], v[1] + rng[2]))
-                continue
-
-            # ( [ ] )
-            if v[0] < rng[0] and v[1] > rng[1]:
-                values.append((v[0], rng[0] - 1))
-                values.append((rng[1] + 1, v[1]))
-                new_values.append((rng[0] + rng[2], rng[1] + rng[2]))
-                continue
-
-            # [ ( ) ]
-            if v[0] >= rng[0] and v[1] <= rng[1]:
-                new_values.append((v[0] + rng[2], v[1] + rng[2]))
-                continue
-
-            # [ ( ] ) 
-            values.append((rng[1] + 1, v[1]))
-            new_values.append((v[0] + rng[2], rng[1] + rng[2]))
-
-    values += new_values
-
-print(min(values)[0])
+print(min(values))
