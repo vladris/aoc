b0e43e391a254a9c72ed47ba9c0902d18366f9d5
Vlad Riscutia <vladris@outlook.com>
2023-12-09 15:14:06 -0600

    Day 9

---
+++ 2023/09/02.py
@@ -0,0 +1,13 @@
+seqs = [list(map(int, line.strip().split(' '))) for
+        line in open('input').readlines()]
+
+def next_value(seq):
+    deltas = []
+    while any([val != 0 for val in seq]):
+        deltas.append(seq)
+        seq = [v[1] - v[0] for v in zip(seq, seq[1:])]
+
+    return sum([seq[-1] for seq in deltas])
+
+
+print(sum([next_value(seq[::-1]) for seq in seqs]))
